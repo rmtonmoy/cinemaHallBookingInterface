9e98ed9f2b69e2eca935486950b04fdc37ca81ef
easonsusan <57599685+easonsusan@users.noreply.github.com>
2021-09-23 14:49:43 -0400

    Starting to build homepage
    Controllers made and carousel started. Working on header and footer

---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/MovieController.java
@@ -0,0 +1,23 @@
+package com.example.CinemaEbookingSystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class MovieController {
+
+    @GetMapping
+    String getMovie(Model model) {
+        model.addAttribute("something", "this is coming from the controller");
+        model.addAttribute("movie", Arrays.asList(
+                new Movie("Movie1"),
+                new Movie("Movie2"),
+                new Movie("Movie3")
+        ));
+        return "movie";
+    }
+
+}
