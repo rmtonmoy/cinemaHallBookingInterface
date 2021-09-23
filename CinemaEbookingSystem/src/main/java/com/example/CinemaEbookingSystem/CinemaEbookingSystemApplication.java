ad0ea2495120fc3700499c7543d6bb37937438b7
easonsusan <57599685+easonsusan@users.noreply.github.com>
2021-09-23 13:19:02 -0400

    Starting out code to build on
    Created using the spring initializer as shown in the tutorial

---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/CinemaEbookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaEbookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaEbookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaEbookingSystemApplication.class, args);
+	}
+
+}
---
+++ CinemaEbookingSystem/src/test/java/com/example/CinemaEbookingSystem/CinemaEbookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaEbookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaEbookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
