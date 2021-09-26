7a3d186950bace53fe7cffcb23bf5cbd4cf52be7
rmtonmoy <rmtonmoy232@gmail.com>
2021-09-26 18:17:38 -0400

    Added registration confirmation page, showed mandatory fields in sign up page

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/MovieController.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/MovieController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MovieController {
@@ -23,6 +26,11 @@ public class MovieController {
     @GetMapping(path = "/signup")
     String signUp(Model model){
         return "signup";
+    }
+
+    @GetMapping(path = "/registration_confirmation")
+    String registrationConfirmationMessage(Model model){
+        return "registrationconfirmation";
     }
 
 }
