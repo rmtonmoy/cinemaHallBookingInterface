06050d6d7e7a24d23d9e0c463faedcbd81ee99e5
James Hyun <jameshyun1230@gmail.com>
2021-10-01 11:20:00 -0400

    Ticket booking page foundations

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/CheckoutController.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/CheckoutController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Arrays;
+
 @Controller
 public class CheckoutController {
     
@@ -16,5 +19,18 @@ public class CheckoutController {
         model.addAttribute("tax", 0.00);
         model.addAttribute("total", 0.00);
         return "checkout";
+    }
+    
+    @GetMapping(path = "/book")
+    String getBook(Model model) {
+        List<Movie> movies = Arrays.asList( // TODO: Read from some external file for the list of movies
+            new Movie("Batman"),
+            new Movie("Dune"),
+            new Movie("Venom"),
+            new Movie("Free Guy"),
+            new Movie("Shang-Chi")
+        );
+        model.addAttribute("movies", movies);
+        return "book";
     }
 }
