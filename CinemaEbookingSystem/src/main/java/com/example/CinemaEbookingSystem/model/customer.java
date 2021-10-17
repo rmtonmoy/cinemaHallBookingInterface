0fc1867e50eeb9a32c4732a8ce4bc73bc99136b0
easonsusan <57599685+easonsusan@users.noreply.github.com>
2021-10-17 12:28:20 -0400

    Importing

---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/model/customer.java
@@ -0,0 +1,11 @@
+package com.example.CinemaEbookingSystem.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GeneratedType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class customer {
+}
---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/model/user.java
@@ -0,0 +1,17 @@
+package com.example.CinemaEbookingSystem.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GeneratedType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class user {
+
+    @Id
+    @GeneratedValue
+    
+}
