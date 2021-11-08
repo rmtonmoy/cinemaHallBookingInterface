eed00f756069741e7dd084d3eec9cdc352fd3417
easonsusan <57599685+easonsusan@users.noreply.github.com>
2021-11-08 16:05:46 -0500

    Email work

---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/EmailService.java
@@ -0,0 +1,19 @@
+package com.example.CinemaEbookingSystem.service;
+
+import com.example.CinemaEbookingSystem.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public interface EmailService {
+    public boolean sendEmail(User user);
+}
---
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/EmailServiceImpl.java
@@ -0,0 +1,62 @@
+package com.example.CinemaEbookingSystem.service;
+
+import com.example.CinemaEbookingSystem.model.User;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    //send email to the user email
+    public boolean sendEmail(User user) {
+        boolean test = false;
+
+        String toEmail = "easonsusan140@gmail.com";
+        String fromEmail = "se.projectgroupc4@gmail.com";
+        String password = "salehpass123";
+
+        try {
+            // your host email smtp server details
+            Properties pr = new Properties();
+            pr.setProperty("mail.smtp.host", "smtp.mail.com");
+            pr.setProperty("mail.smtp.port", "587");
+            pr.setProperty("mail.smtp.auth", "true");
+            pr.setProperty("mail.smtp.starttls.enable", "true");
+            pr.put("mail.smtp.socketFactory.port", "587");
+            pr.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+            //get session to authenticate the host email address and password
+            Session session = Session.getInstance(pr, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            });
+
+            //set email message details
+            Message mess = new MimeMessage(session);
+
+            //set from email address
+            mess.setFrom(new InternetAddress(fromEmail));
+            //set to email address or destination email address
+            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+
+            //set email subject
+            mess.setSubject("User Email Verification");
+
+            //set message text
+            mess.setText("Registered successfully.Please verify your account using this code: " + user.getCode());
+            //send the message
+            Transport.send(mess);
+            test=true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return test;
+    }
+}
