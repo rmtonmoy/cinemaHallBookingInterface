76a0449c5c719516ae5e56c75d5580763b3c06ad
easonsusan <57599685+easonsusan@users.noreply.github.com>
2021-11-18 14:17:10 -0500

    Setting up promotion controller

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/controller/PromotionController.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/controller/PromotionController.java
@@ -1,5 +1,6 @@
 package com.example.CinemaEbookingSystem.controller;
 
+import com.example.CinemaEbookingSystem.service.EmailService;
 import com.example.CinemaEbookingSystem.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PromotionController {
     @Autowired
     private PromotionService promotionService;
+    @Autowired
+    private EmailService emailService;
 /*
     @GetMapping(path = "/create_db")
     public String dummyPage(Model model){
