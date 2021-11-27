89dcfac38667abca4c3a2716f5d85c56e4278441
rmtonmoy <rmtonmoy232@gmail.com>
2021-11-27 06:56:19 -0400

    Finished coding the feature

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/TicketServiceImpl.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/TicketServiceImpl.java
@@ -1,16 +1,15 @@
 package com.example.CinemaEbookingSystem.service;
 
-import com.example.CinemaEbookingSystem.model.MovieInfo;
-import com.example.CinemaEbookingSystem.model.OneShow;
-import com.example.CinemaEbookingSystem.model.Theater;
-import com.example.CinemaEbookingSystem.model.Ticket;
+import com.example.CinemaEbookingSystem.model.*;
 import com.example.CinemaEbookingSystem.repository.TicketRepository;
 import org.apache.tomcat.jni.Local;
+import org.hibernate.dialect.Ingres9Dialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.CollationElementIterator;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class TicketServiceImpl implements TicketService {
@@ -85,5 +84,86 @@ public class TicketServiceImpl implements TicketService {
         }
 
         return true;
+    }
+
+    @Override
+    public List<List<String>> generateSchedule() {
+        List<List<String>> scheduleTable = new ArrayList<List<String>>();
+        List<String> headers = new ArrayList<>();
+
+        Calendar today = Calendar.getInstance();
+        List<Integer> importantTimes = new ArrayList<>();
+        List<Ticket> ticketList = ticketRepository.findAll();
+        headers.add("Date/Time");
+
+        for(int i = 1; i <= 7; i++){
+            Integer day = today.get(Calendar.DAY_OF_MONTH);
+            Integer month = today.get(Calendar.MONTH) + 1;
+            Integer year = today.get(Calendar.YEAR);
+
+            String formatedDate = month.toString() + "/" + day.toString() + "/" + year.toString();
+            for(Ticket ticket : ticketList){
+                OneShow oneShow = ticket.getShow();
+                ShowTime showTime = oneShow.getShowTime();
+                String date = showTime.getDate();
+                if(date.equals(formatedDate)){
+                    importantTimes.add(showTime.getStartingTime());
+                }
+            }
+
+            headers.add(formatedDate);
+            today.add(Calendar.DATE, 1);
+        }
+        for(int i = 1; i <= 7; i++){
+            today.add(Calendar.DATE, -1);
+        }
+
+        scheduleTable.add(headers);
+        Set<Integer> setOfImportantTimes = new HashSet<>(importantTimes);
+        importantTimes = new ArrayList<>(setOfImportantTimes);
+
+
+        for(Integer particularStartingTime : importantTimes){
+            ShowTime dummyShowTime = new ShowTime();
+            dummyShowTime.setStartingTime(particularStartingTime);
+            String formatedStartingTime = dummyShowTime.getStartingTimeString();
+
+            List<String> currentRow = new ArrayList<>();
+            currentRow.add(formatedStartingTime);
+
+            for(int i = 1; i <= 7; i++){
+                Integer day = today.get(Calendar.DAY_OF_MONTH);
+                Integer month = today.get(Calendar.MONTH) + 1;
+                Integer year = today.get(Calendar.YEAR);
+                String entry = "";
+                Set<String> movieSet = new HashSet<>();
+
+                String formatedDate = month.toString() + "/" + day.toString() + "/" + year.toString();
+                for(Ticket ticket : ticketList){
+                    OneShow oneShow = ticket.getShow();
+                    ShowTime showTime = oneShow.getShowTime();
+                    String date = showTime.getDate();
+                    if(date.equals(formatedDate) && showTime.getStartingTime() == particularStartingTime){
+                        if(movieSet.contains(ticket.getShow().getMovieInfo().getTitle())){
+                            continue;
+                        }
+                        movieSet.add(ticket.getShow().getMovieInfo().getTitle());
+
+                        if(entry.isEmpty() == false){
+                            entry = entry + ", ";
+                        }
+                        entry = entry + ticket.getShow().getMovieInfo().getTitle();
+                    }
+                }
+                
+                today.add(Calendar.DATE, 1);
+                currentRow.add(entry);
+            }
+            for(int i = 1; i <= 7; i++){
+                today.add(Calendar.DATE, -1);
+            }
+            scheduleTable.add(currentRow);
+        }
+        return scheduleTable;
     }
 }
