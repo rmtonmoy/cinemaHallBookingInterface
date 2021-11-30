7b36ef5c124b52f088fbc0a2b458cc8faeb290e5
James Hyun <jameshyun1230@gmail.com>
2021-11-30 17:43:09 -0500

    Laid groundwork for searching by date (DYSFUNCTIONAL)
    For some reason I can't seem to be able to extract the date from form submissions

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/dto/MovieSearchDto.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/dto/MovieSearchDto.java
@@ -1,12 +1,19 @@
 package com.example.CinemaEbookingSystem.dto;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
 public class MovieSearchDto {
+    
+    private DateFormat dateFormat = DateFormat.getDateInstance();
     
     // If you make any of these private, I will be very angry
     // -James
     public String title;
     public String cat; // category
     public String status;
+    public String date; // has to be string since it's delivered from the frontend as a string
     
     // I cannot believe thymeleaf is forcing me to make some blasted one-line getter/setter pairs, jfc
     // I'm not coming within 500 trillion light years of this """""""""""software""""""""""" after this class
@@ -29,6 +36,21 @@ public class MovieSearchDto {
     }
     public void setStatus(String s) {
         status = s;
+    }
+    
+    public Date getDate() {
+        if (date != null) {
+            try {
+                Date d = dateFormat.parse(date);
+                return d;
+            } catch(ParseException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+    public void setDate(String d) {
+        date = d;
     }
     
 }
