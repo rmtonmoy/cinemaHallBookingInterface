c560383538a2be33786bb1e90ffe7cd2c22cbb26
ScarMag <oscar.magana00@gmail.com>
2021-12-01 19:47:19 -0500

    Added change password feature for admins

--- CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/AdminServiceImpl.java
+++ CinemaEbookingSystem/src/main/java/com/example/CinemaEbookingSystem/service/AdminServiceImpl.java
@@ -1,9 +1,8 @@
 package com.example.CinemaEbookingSystem.service;
 
+import com.example.CinemaEbookingSystem.dto.PasswordDto;
 import com.example.CinemaEbookingSystem.dto.UserRegistrationDto;
 import com.example.CinemaEbookingSystem.model.Admin;
-import com.example.CinemaEbookingSystem.model.Customer;
-import com.example.CinemaEbookingSystem.model.PaymentCard;
 import com.example.CinemaEbookingSystem.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,4 +43,15 @@ public class AdminServiceImpl implements AdminService{
             return 2;
     }
 
+    @Override
+    public boolean isCorrectPassword(String email, PasswordDto passwordDto) {
+        Admin admin = adminRepository.findByEmail(email);
+
+        if (passwordDto.getOldPassword().equals(admin.getPassword())) {
+            admin.setPassword(passwordDto.getNewPassword());
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
