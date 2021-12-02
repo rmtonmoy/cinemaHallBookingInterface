dfdf076eed02e6b4d3c70a533008421ee70d1130
James Hyun <jameshyun1230@gmail.com>
2021-12-02 01:13:05 -0500

    Laid groundwork for booking frontend

--- CinemaEbookingSystem/src/main/resources/static/book.js
+++ CinemaEbookingSystem/src/main/resources/static/book.js
@@ -3,6 +3,8 @@
 "use strict";
 
 $(document).ready(function() {
+    let ticketIndex = -1;
+    
     $(".movieRadioBtn").change(function() {
         let btn = $(this)
         let id  = btn.attr("data-movie");
@@ -33,9 +35,72 @@ $(document).ready(function() {
         });
     });
     
+    $("#addTicketBtn").click(function() {
+        ticketIndex++;
+        if (ticketIndex == 0) {
+            $("#rmTicketBtn").removeClass("invisible");
+        }
+        $("#ticketContainer").append(createTicket(ticketIndex));
+    });
+    
+    $("#rmTicketBtn").click(function() {
+        $("#ticket" + ticketIndex).remove();
+        ticketIndex--;
+        if (ticketIndex == -1) {
+            $("#rmTicketBtn").addClass("invisible");
+        }
+    });
+    
     $("option").each(function() {
         $(this).hide();
     });
     $("#showDate").val(0);
     $("#showTime").val(0);
 });
+
+function createTicket(num) {
+    return `<div id="ticket${num}" class="form-group">
+    <h3>Ticket ${num + 1}</h3>
+    <label for="typeOfTicket">Ticket Type:</label>
+    <select th:field="*{tickets[${num}].type}" name="typeOfTicket" id="typeOfTicket" class="form-control col-1">
+        <option value="adult">Adult</option>
+        <option value="child">Child</option>
+    </select>
+
+    <h3>Seat:</h3>
+    <h4>Available Seating</h4>
+
+    <div class="container">
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">A1</button>
+            <button type="button" class="btn btn-primary">A2</button>
+            <button type="button" class="btn btn-primary">A3</button>
+            <button type="button" class="btn btn-primary">A4</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">B1</button>
+            <button type="button" class="btn btn-primary">B2</button>
+            <button type="button" class="btn btn-primary">B3</button>
+            <button type="button" class="btn btn-primary">B4</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">C1</button>
+            <button type="button" class="btn btn-primary">C2</button>
+            <button type="button" class="btn btn-primary">C3</button>
+            <button type="button" class="btn btn-primary">C4</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">D1</button>
+            <button type="button" class="btn btn-primary">D2</button>
+            <button type="button" class="btn btn-primary">D3</button>
+            <button type="button" class="btn btn-primary">D4</button>
+        </div>
+    </div>
+</div>`;
+}
