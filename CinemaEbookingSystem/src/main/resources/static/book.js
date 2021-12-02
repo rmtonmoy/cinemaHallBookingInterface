31557a06c773abeb4812a3ad31b12cada618991b
James Hyun <jameshyun1230@gmail.com>
2021-12-01 23:38:10 -0500

    Showing times dropdown fills based on selected day

--- CinemaEbookingSystem/src/main/resources/static/book.js
+++ CinemaEbookingSystem/src/main/resources/static/book.js
@@ -2,32 +2,40 @@
 // Handles swapping out booking time options based on movie selected
 "use strict";
 
-function showHide(element, id) {
-    if (element.attr("data-movie") == id) {
-        element.show();
-    } else {
-        element.hide();
-    }
-}
-
 $(document).ready(function() {
     $(".movieRadioBtn").change(function() {
-        let id = $(this).attr("data-movie");
+        let btn = $(this)
+        let id  = btn.attr("data-movie");
         $("#showDate > option").each(function() {
             console.log("showing show date");
-            showHide($(this), id);
-        });
-        $("#showTime > option").each(function() {
-            console.log("showing show time");
-            showHide($(this), id);
+            if ($(this).attr("data-movie") == id) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
         });
         $("#showDate").val(0);
         $("#showTime").val(0);
     });
     
+    $("#showDate").change(function() {
+        let selected = $(this).find(":selected");
+        let id       = selected.attr("data-movie");
+        $("#showTime > option").each(function() {
+            console.log("showing show time");
+            let opt = $(this);
+            if (opt.attr("data-movie") == id && opt.attr("data-showing-date") == selected.attr("data-showing-date")) {
+                opt.show();
+            } else {
+                opt.hide();
+            }
+            $("#showTime").val(0);
+        });
+    });
+    
     $("option").each(function() {
         $(this).hide();
-    })
+    });
     $("#showDate").val(0);
     $("#showTime").val(0);
 });
